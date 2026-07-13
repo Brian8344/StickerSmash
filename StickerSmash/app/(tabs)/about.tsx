56c4b50a8d889d8a42418bf24c73cfdd6a8e0ee6
Brian8344 <hubahibbri@gmail.com>
2026-07-13 14:12:20 -0700

    3rd commit

--- StickerSmash/app/(tabs)/about.tsx
+++ StickerSmash/app/(tabs)/about.tsx
@@ -11,7 +11,7 @@ export default function AboutScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2208b4',
+    backgroundColor: '#011a3a',
     justifyContent: 'center',
     alignItems: 'center',
   },
